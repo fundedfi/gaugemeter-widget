0707a2fb36ab1fb7fbd175df9f3f02d6152c7a5f
Funded Financial <131178852+fundedfi@users.noreply.github.com>
2025-06-11 12:51:33 -0700

    💥 Proper UI sync: Funding Gauge 2.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
-import GaugeDemo from './components/GaugeDemo.jsx';
+import FundingGauge from './components/FundingGauge.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GaugeDemo />
+    <FundingGauge />
   </React.StrictMode>
 );
